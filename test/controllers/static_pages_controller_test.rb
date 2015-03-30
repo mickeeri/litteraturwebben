3a0eef5047911a70cf554fbac059792aac6756f2
me222wm <me222wm@student.lnu.se>
2015-03-30 17:11:01 +0200

    Add contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,24 +2,34 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
+  def setup
+  	@base_title = "Litteraturappen"
+  end
+
   # Test expect home and help to be ordinary web pages accessed using GET request.
   test "should get home" do
     get :home
     assert_response :success
     # Looks if there is a title tag with that name.
-    assert_select "title", "Hem | Litteraturappen"
+    assert_select "title", "Hem | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Hjälp | Litteraturappen"
+    assert_select "title", "Hjälp | #{@base_title}"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "Om | Litteraturappen"
+  	assert_select "title", "Om | #{@base_title}"
+  end
+
+  test "should get contact" do
+  	get :contact
+  	assert_response :success
+  	assert_select "title", "Kontakt | #{@base_title}"
   end
 
 end
