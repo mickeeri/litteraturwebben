c8a596f51b5c361fb68ade54456eeebe7b762b80
me222wm <me222wm@student.lnu.se>
2015-03-30 16:22:58 +0200

    Made basic static pages with tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  # Test expect home and help to be ordinary web pages accessed using GET request.
   test "should get home" do
     get :home
     assert_response :success
+    # Looks if there is a title tag with that name.
+    assert_select "title", "Hem | Litteraturappen"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Hjälp | Litteraturappen"
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+  	assert_select "title", "Om | Litteraturappen"
   end
 
 end
