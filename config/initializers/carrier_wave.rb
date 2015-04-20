2db38e82994762b2736a0f2f8c3c2f5d2ed21cd8
me222wm <me222wm@student.lnu.se>
2015-04-20 18:57:58 +0200

    Typo

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
 # Amazon Web Service config.
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog.credentials = {
+    config.fog_credentials = {
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY_ID'],
       :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
