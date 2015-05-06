e58f5a0b183fb4f5498b3603cd38dad652e60df7
me222wm <me222wm@student.lnu.se>
2015-05-06 21:10:08 +0200

    Changed navbar and Css.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
 			"#{page_title} | #{base_title}"
 		end
 	end
+
+	# Adds class active to active navbar links.
+	def is_active?(link_path)
+		current_page?(link_path) ? "active" : ""
+	end
 end
