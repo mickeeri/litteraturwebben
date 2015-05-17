957e47ad36ce90970e72d338c771a9538184017d
me222wm <me222wm@student.lnu.se>
2015-05-17 19:25:52 +0200

    Added another initializer.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,2 +1,1 @@
-Searchkick.client = Elasticsearch::Client.new(hosts: ["localhost:9200", "localhost:9201"], retry_on_failure: true)
-Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
+#Searchkick.client = Elasticsearch::Client.new(hosts: ["localhost:9200", "localhost:9201"], retry_on_failure: true)
