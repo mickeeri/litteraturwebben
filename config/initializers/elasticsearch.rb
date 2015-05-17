9ef60f50ab3c0196a7cf0cc65edd885cae8d8e9c
me222wm <me222wm@student.lnu.se>
2015-05-17 19:15:51 +0200

    Bonsai url in elasticsearch initializer.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,2 @@
 Searchkick.client = Elasticsearch::Client.new(hosts: ["localhost:9200", "localhost:9201"], retry_on_failure: true)
+Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
