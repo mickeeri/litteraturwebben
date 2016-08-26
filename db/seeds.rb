512b9e18d15e8198f17abdd8de57e62fb54624af
me222wm <me222wm@student.lnu.se>
2016-08-26 14:39:42 +0200

    Modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,93 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# !create method raises exception for invalid user rather than returning false.
-
 Genre.create(genre: "Skönlitteratur")
 Genre.create(genre: "Lyrik")
 Genre.create(genre: "Dramatik")
 Genre.create(genre: "Faktalitteratur")
 Genre.create(genre: "Essäer")
 
-# Using Faker gem to make 99 example users.
-99.times do |n|
-	name = Faker::Name.name
-	email = "example-#{n+1}@mail.com"
-	password = "password"
-	User.create!(name: name,
-				email: email,
-				password: password,
-				password_confirmation: password)
+# Users
+99.times { FactoryGirl.create :user }
+
+# Admin user
+User.create(name: "Mikael Eriksson",
+            email: "admin@test.com",
+            password: "password",
+            password_confirmation: "password",
+            admin: true)
+
+# Authors
+10.times do
+  FactoryGirl.create :author
 end
 
-Author.create!(name: "August Strindberg",
-	about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	Maecenas mollis vestibulum elementum. Curabitur et pretium lorem.
-	Nulla malesuada, purus quis sodales hendrerit, turpis lacus imperdiet arcu,
-	quis venenatis dui mauris at enim. Praesent at ligula a felis iaculis gravida nec at lectus.
-	Nullam nec tellus malesuada, feugiat nisl sed, elementum lectus.
-	Integer venenatis, erat in condimentum cursus, lectus augue vulputate tellus,
-	et dictum elit risus a quam. Aliquam erat volutpat. Proin vel eros in mauris ornare ornare in id turpis.")
+Author.create!(
+  name: "August Strindberg",
+  yearofbirth: 1787,
+  about: Faker::Lorem.sentence(30)
+)
 
 Author.create!(name: "Selma Lagerlöf",
-	about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	Maecenas mollis vestibulum elementum. Curabitur et pretium lorem.
-	Nulla malesuada, purus quis sodales hendrerit, turpis lacus imperdiet arcu,
-	quis venenatis dui mauris at enim. Praesent at ligula a felis iaculis gravida nec at lectus.
-	Nullam nec tellus malesuada, feugiat nisl sed, elementum lectus.
-	Integer venenatis, erat in condimentum cursus, lectus augue vulputate tellus,
-	et dictum elit risus a quam. Aliquam erat volutpat. Proin vel eros in mauris ornare ornare in id turpis.")
+               yearofbirth: 1787,
+               about: Faker::Lorem.sentence(30))
 
 Author.create!(name: "Hjalmar Söderberg",
-	about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	Maecenas mollis vestibulum elementum. Curabitur et pretium lorem.
-	Nulla malesuada, purus quis sodales hendrerit, turpis lacus imperdiet arcu,
-	quis venenatis dui mauris at enim. Praesent at ligula a felis iaculis gravida nec at lectus.
-	Nullam nec tellus malesuada, feugiat nisl sed, elementum lectus.
-	Integer venenatis, erat in condimentum cursus, lectus augue vulputate tellus,
-	et dictum elit risus a quam. Aliquam erat volutpat. Proin vel eros in mauris ornare ornare in id turpis.")
+               yearofbirth: 1787,
+               about: Faker::Lorem.sentence(30))
 
 Author.create!(name: "Viktor Rydberg",
-	about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	Maecenas mollis vestibulum elementum. Curabitur et pretium lorem.
-	Nulla malesuada, purus quis sodales hendrerit, turpis lacus imperdiet arcu,
-	quis venenatis dui mauris at enim. Praesent at ligula a felis iaculis gravida nec at lectus.
-	Nullam nec tellus malesuada, feugiat nisl sed, elementum lectus.
-	Integer venenatis, erat in condimentum cursus, lectus augue vulputate tellus,
-	et dictum elit risus a quam. Aliquam erat volutpat. Proin vel eros in mauris ornare ornare in id turpis.")
+               yearofbirth: 1787,
+               about: Faker::Lorem.sentence(30))
 
 Author.create!(name: "Carl Jonas Love Almqvist",
-	about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	Maecenas mollis vestibulum elementum. Curabitur et pretium lorem.
-	Nulla malesuada, purus quis sodales hendrerit, turpis lacus imperdiet arcu,
-	quis venenatis dui mauris at enim. Praesent at ligula a felis iaculis gravida nec at lectus.
-	Nullam nec tellus malesuada, feugiat nisl sed, elementum lectus.
-	Integer venenatis, erat in condimentum cursus, lectus augue vulputate tellus,
-	et dictum elit risus a quam. Aliquam erat volutpat. Proin vel eros in mauris ornare ornare in id turpis.")
+               yearofbirth: 1787,
+               about: Faker::Lorem.sentence(30))
 
 Author.create!(name: "Erik Lindegren",
-	about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	Maecenas mollis vestibulum elementum. Curabitur et pretium lorem.
-	Nulla malesuada, purus quis sodales hendrerit, turpis lacus imperdiet arcu,
-	quis venenatis dui mauris at enim. Praesent at ligula a felis iaculis gravida nec at lectus.
-	Nullam nec tellus malesuada, feugiat nisl sed, elementum lectus.
-	Integer venenatis, erat in condimentum cursus, lectus augue vulputate tellus,
-	et dictum elit risus a quam. Aliquam erat volutpat. Proin vel eros in mauris ornare ornare in id turpis.")
+               yearofbirth: 1787,
+               about: Faker::Lorem.sentence(50))
 
 Author.create!(name: "Victoria Benedictsson",
-	about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	Maecenas mollis vestibulum elementum. Curabitur et pretium lorem.
-	Nulla malesuada, purus quis sodales hendrerit, turpis lacus imperdiet arcu,
-	quis venenatis dui mauris at enim. Praesent at ligula a felis iaculis gravida nec at lectus.
-	Nullam nec tellus malesuada, feugiat nisl sed, elementum lectus.
-	Integer venenatis, erat in condimentum cursus, lectus augue vulputate tellus,
-	et dictum elit risus a quam. Aliquam erat volutpat. Proin vel eros in mauris ornare ornare in id turpis.")
+               yearofbirth: 1787,
+               about: Faker::Lorem.sentence(30))
 
+# Books
+Author.all.each do |author|
+  genre = Genre.offset(rand(Genre.count)).first
+  book = FactoryGirl.build :book, genre: genre
 
-
-
+  author.books << book
+end
